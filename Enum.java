f5d6f201cf0c56bf3ee754a859a1b2505c4039df
Nikita (silvestr) <101042426+silvestrr@users.noreply.github.com>
2022-05-15 20:50:44 +0300

    Add files via upload

---
+++ Enum.java
@@ -0,0 +1,24 @@
+public class Enum {
+    public static void main(String [] args){
+//        testEnum animal = testEnum.CAT;
+//        switch (animal){
+//            case CAT:
+//                System.out.println("Its a cat!");
+//                break;
+//            case DOG:
+//                System.out.println("Its a dog!");
+//            case FROG:
+//                System.out.println("Its a frog");
+//            break;
+//            default:
+//                System.out.println("Its not an animals");
+//        }
+    Season season = Season.SUMMER;
+        switch (season){
+            case SUMMER:
+            System.out.println("Its a summer");
+            break;
+        }
+    }
+}
+
--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
+
 public class Main {
+    public static String calc(String input) {
+        return input;
+    }
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Введи арифметическое действие");
@@ -11,28 +16,28 @@ public class Main {
         int n2 = 0;
         int answer = 0;
         int count = 0;
-        if (example.length < 3){
+        if (example.length < 3) {
             System.out.println("throws Exception //т.к. строка не является математической операцией");
         }
-        for (int i=0; number.length != i; i++) {
+        for (int i = 0; number.length != i; i++) {
             if (example[0].equals(number[i]))
                 n1 = i + 1;
-            }
-        if (n1 == 0) {
-                testEnum test = testEnum.valueOf(example[0]);
-                n1 = test.getNumber();
-                count += 1;
         }
-        for (int i=0; number.length != i; i++) {
-                if (example[2].equals(number[i]))
-                    n2 = i + 1;
-            }
-        if (n2 == 0){
+        if (n1 == 0) {
+            testEnum test = testEnum.valueOf(example[0]);
+            n1 = test.getNumber();
+            count += 1;
+        }
+        for (int i = 0; number.length != i; i++) {
+            if (example[2].equals(number[i]))
+                n2 = i + 1;
+        }
+        if (n2 == 0) {
             testEnum test = testEnum.valueOf(example[2]);
             n2 = test.getNumber();
             count += 1;
-            }
-            switch (example[1]){
+        }
+        switch (example[1]) {
             case "+":
                 answer = (n1 + n2);
                 break;
@@ -45,29 +50,22 @@ public class Main {
             case "/":
                 answer = (n1 / n2);
                 break;
-                default:
-                    System.out.println("throws Exception //т.к. строка не является математической операцией");
-            }
-
-            if (count == 0 && example.length < 4) {
+            default:
+                System.out.println("throws Exception //т.к. строка не является математической операцией");
+        }
+        if (count == 0 && example.length < 4) {
             System.out.println(answer);
-        }
-        else if(count == 2 && example.length < 4 && answer >0) {
+        } else if (count == 2 && example.length < 4 && answer > 0) {
             System.out.println(testEnum.values()[answer - 1]);
-        }
-        else if (count == 1 && example.length < 4){
-                System.out.println("throws Exception //т.к. используются одновременно разные системы счисления");
-            }
-        else if (answer < 1){
+        } else if (count == 1 && example.length < 4) {
+            System.out.println("throws Exception //т.к. используются одновременно разные системы счисления");
+        } else if (answer < 1) {
             System.out.println("throws Exception //т.к. в римской системе нет отрицательных чисел");
-        }
-        else{
+        } else {
             System.out.println("throws Exception //т.к. формат математической операции не удовлетворяет заданию - два операнда и один оператор (+, -, /, *)");
         }
     }
 }
-
-
 
 
 
